128911c83c3ddfc0b55dafe5e0133fc46af4e896
MikeSw <mike@sapiensworks.com>
2012-06-02 10:29:33 +0300

    added DomainEvents helper class
    static helper to handle domain events for the times when you want a
    'quick and dirty' way

---
+++ src/DomainEventsToolkit/DomainEvents.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using DomainEvents.Managers;
+
+namespace DomainEvents
+{
+    public static class DomainEvents
+    {
+        [ThreadStatic] private static LocalDomainEventsManager _events;
+        
+        static LocalDomainEventsManager Events
+        {
+            get
+            {
+                if (_events==null)
+                {
+                    _events = new LocalDomainEventsManager();
+                }
+                return _events;
+            }
+        }
+        
+
+        public static IDisposable RegisterHandler<T>(IHandleDomainEvent<T> handler) where T:IDomainEvent
+        {
+            return Events.RegisterHandler(handler);
+        }
+        
+        public static IDisposable RegisterHandler<T>(Action<T> handler) where T:IDomainEvent
+        {
+            return Events.RegisterHandler(handler);
+        }
+
+        public static void PublishEvent<T>(T evnt) where T:IDomainEvent
+        {
+            Events.Publish(evnt);
+        }
+
+        public static void RemoveHandlers()
+        {
+            Events.Clear();
+        }
+    }
+}
